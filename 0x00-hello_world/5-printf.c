9777f36ac27cc7c4c67d69d0d4067e26eba893f5
Chidi-onuigbo Chikaodili <econuigbo@yahoo.com>
2023-08-18 10:26:09 +0100

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main- a line that outputs Hello world using printf
- * return: 0(sucess)
+ * Return: 0(Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf(" with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
