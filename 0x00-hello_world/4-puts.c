ffb9a3000e10f5fb0f0edff2a70412c09136f436
Chidi-onuigbo Chikaodili <econuigbo@yahoo.com>
2023-08-18 06:08:28 +0100

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * return: 0 (sucess)
+ */
+int main(void)
+{
+	char Mystr []= "\"Programming is like building a multilingual puzzle";
+	puts(Mystr);
+
+	Return (0);
+}
