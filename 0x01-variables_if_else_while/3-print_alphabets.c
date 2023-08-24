26d0c40c40ab974953f5f82d9cdca30dec775942
Chidi-onuigbo Chikaodili <econuigbo@yahoo.com>
2023-08-24 15:31:22 +0100

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *  * main - code is in the main
+ *   *
+ *    * Return: success(0)
+ *     */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+                putchar(ch);
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
