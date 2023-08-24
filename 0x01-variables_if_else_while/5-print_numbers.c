2800ed1be2c0748737b10046a4597ea9c864f135
Chidi-onuigbo Chikaodili <econuigbo@yahoo.com>
2023-08-24 21:30:18 +0100

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main code
+ *
+ * Return: Success (0)
+ */
+int main(void)
+{
+	int nani;
+
+	for (nani = 0; nani < 10; nani++)
+		putchar(nani);
+	putchar('\n');
+	return (0);
+}
