47dda54640595f3e94dc289564ab6b53a12bb9b4
Chidi-onuigbo Chikaodili <econuigbo@yahoo.com>
2023-08-28 13:12:54 +0100

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main code
+ *
+ * Return: Success (0)
+ */
+int main(void)
+{
+	int nani;
+
+	for (nani = 48; nani < 58; nani++)
+	{
+		putchar(nani);
+	}
+
+	putchar('\n');
+	return (0);
+}
