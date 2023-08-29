724199220ca322329a48cd50c514a801b3baa7ad
Chidi-onuigbo Chikaodili <econuigbo@yahoo.com>
2023-08-29 11:39:16 +0100

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - printing hexadecimal
+ *
+ * Return: Success(0)
+ */
+int main(void)
+{
+	int x;
+	char y;
+
+	for (x = 0; x < 10; x++)
+		putchar(x + '0');
+	for (y = 'a'; y < 'g'; y++)
+		putchar(y);
+	putchar('\n');
+	return (0);
+}
