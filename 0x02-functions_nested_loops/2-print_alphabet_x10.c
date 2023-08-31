5ac0e404f2858a2dea08b2565fb443640935f212
Chidi-onuigbo Chikaodili <econuigbo@yahoo.com>
2023-08-31 16:27:48 +0100

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function used
+ *
+ * main - where function is used
+ *
+ * Return: Success(void)
+ */
+void print_alphabet_x10(void)
+{
+	int y;
+	int z;
+
+	for (y = 0; y <= 10; y++)
+	{
+		for (z = 'a'; z <= 'z'; z++)
+		{
+			putchar(z);
+		}
+		putchar('\n');
+	}
+}
+
