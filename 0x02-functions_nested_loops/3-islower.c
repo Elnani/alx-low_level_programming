a897ed3e2026fd0aa25a9eaf6e12fd00d2662096
Chidi-onuigbo Chikaodili <econuigbo@yahoo.com>
2023-09-01 15:03:47 +0100

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - checking for small alphabets
+ * @c: the character to be checked
+ * Return: Success 1
+ */
+
+int _islower(int c)
+{
+
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
