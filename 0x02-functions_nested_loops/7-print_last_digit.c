d782eee6d06c79197652484bcbcc91dedc03809e
Chidi-onuigbo Chikaodili <econuigbo@yahoo.com>
2023-09-02 07:22:33 +0100

    7-last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,13 @@
 int print_last_digit(int c)
 {
 	int last_dig;
+
 	last_dig = c % 10;
 	if (c < 0)
 	{
 		last_dig = last_dig * -1;
 	}
-	return last_dig;
+	_putchar (last_dig + '0');
+	return (last_dig);
 }
-	
+
