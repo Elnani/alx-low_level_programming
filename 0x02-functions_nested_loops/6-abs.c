bd6a5cf9ebbab13375e30070febabd92b593066c
Chidi-onuigbo Chikaodili <econuigbo@yahoo.com>
2023-09-02 06:38:48 +0100

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _abs - returning an absolute integer
+ * @c: variable containment
+ * Return: Success (0)
+ */
+int _abs(int c)
+{
+
+	if (c < 0)
+	{
+		return (c * (-1));
+	}
+	else if (c > 0)
+	{
+		return (c);
+	}
+	else
+	{
+		return (c);
+	}
+}
