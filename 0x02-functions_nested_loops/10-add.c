d2d3666040e9bd3e51be67484f6a4ad537415cd0
Chidi-onuigbo Chikaodili <econuigbo@yahoo.com>
2023-09-05 11:41:51 +0100

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - addition of two int.
+ * @m: variable to be considered.
+ * @n:variable to be considered.
+ * Return: sum (0).
+ */
+int add(int m, int n)
+{
+	int v;
+
+	v = m + n;
+	return (v);
+}
+
