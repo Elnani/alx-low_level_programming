2daeba9ad052b26ad72e22a1e60d173ab4633a22
Chidi-onuigbo Chikaodili <econuigbo@yahoo.com>
2023-09-10 16:12:31 +0100

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - a program to print finobacci sequence of even numbers
+ *
+ * Return: Success (0)
+ */
+int main(void)
+{
+	unsigned long first, second, sum;
+	float sum_total;
+	while(1)
+	{
+		sum = first + second;
+		if ( sum > 4000000)
+		break;
+
+		if ((sum % 2) == 0)
+		{
+			sum_total += sum;
+		}
+		first = second;
+		second = sum;
+	}
+	printf("%.0f, " sum_total);
+	return (0);
+}
+
