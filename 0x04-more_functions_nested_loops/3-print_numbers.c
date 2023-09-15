80a9a3f6df29c8e89dce9ba3eeb79b64dcd25862
Chidi-onuigbo Chikaodili <econuigbo@yahoo.com>
2023-09-15 13:38:17 +0100

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,14 @@
-#include "main.h"
+include "main.h"
+
 /**
  * print_numbers - Prints the numbers from 0-9.
  */
 void print_numbers(void)
 {
-	int j;
+	int num;
 
-	j = 0;
-	while (j <= 9)
-	{
-		_putchar (j + '0');
-		j++;
-	}
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+
 	_putchar('\n');
-	return;
 }
